01327c4666f8bb38e9dac50865aa37f3538f1321
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-16 16:06:08 -0700

    Update chat.php

--- chat.php
+++ chat.php
@@ -10,6 +10,8 @@ if (mysqli_connect_errno()) {
    Please try again later.</p>';
    exit;
 }
+
+
 //helper funtion to replace get_results() if without mysqlnd 
 function get_result( $Statement ) {
     $RESULT = array();
@@ -32,20 +34,17 @@ try {
     $action = isset($_SERVER['REQUEST_METHOD']) && ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'send' : 'poll';
     switch($action) {
         case 'poll':
-           $query = "SELECT * FROM `chatlog` WHERE date_created >= ".$lastPoll;
+           $query = "SELECT * FROM chatlog WHERE date_created >= ".$lastPoll;
            $stmt = $db->prepare($query);
            $stmt->execute();
-           $stmt->bind_result($id, $message, $session_id, $date_created, $chat_usrname, $color);
+           $stmt->bind_result($id, $message, $session_id, $date_created, $chat_username, $color);
            $result = get_result( $stmt);
            $newChats = [];
            while($chat = array_shift($result)) {
                
-               if($session_id == $chat['sent_by']) {
-                  $chat['sent_by'] = 'self';
-               } else {
-                  $chat['sent_by'] = 'other';
-               }
-             
+               if($session_id == $chat['session_id']) {
+                  $chat['chat_username'] = 'self';
+               }              
                $newChats[] = $chat;
             }
            $_SESSION['last_poll'] = $currentTime;
@@ -56,14 +55,15 @@ try {
            ]);
            exit;
         case 'send':
-            $chat_usrname = isset($_POST['chat_usrname']) ? $_POST['chat_usrname'] : ' ';
-            $chat_usrname = strip_tags($chat_usrname);
+            $chat_username = isset($_POST['chat_username']) ? $_POST['chat_username'] : '';
+            $chat_username = strip_tags($chat_username); 
             $message = isset($_POST['message']) ? $_POST['message'] : '';            
             $message = strip_tags($message);
+                        
             $color = isset($_POST['color']) ? $_POST['color'] : '';
-            $query = "INSERT INTO `chatlog` (message, sent_by, date_created, chat_usrname, color) VALUES(?, ?, ?, ?, ?)";
+            $query = "INSERT INTO chatlog (message, session_id, date_created, chat_username, color) VALUES(?, ?, ?, ?, ?)";
             $stmt = $db->prepare($query);
-            $stmt->bind_param('ssiss', $message, $session_id, $currentTime, $chat_usrname, $color); 
+            $stmt->bind_param('ssiss', $message, $session_id, $currentTime, $chat_username, $color); 
             $stmt->execute(); 
             print json_encode(['success' => true]);
             exit;
